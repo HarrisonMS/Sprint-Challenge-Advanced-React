90d7b9a59ad68bed13ae2a33c5e861e52ade390a
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-11-01 11:28:15 -0700

    mvp i think

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as rtl from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+// import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 import { isMainThread } from 'worker_threads';
 import Display from "./components/Display";
 import PlayerCard from "./components/PlayerCard";
+import useDarkMode from "./components/hooks/useDarkMode";
+import useLocalStorage from "./components/hooks/useLocalStorage"
 
 
 test('renders PlayerCard component', () => {
   const wrapper = rtl.render(<PlayerCard />);
+  console.log(wrapper.debug());
+});
+
+
+test('renders localstorage', () => {
+  const wrapper = rtl.render(<useLocalStorage/>);
+  console.log(wrapper.debug());
+});
+
+test('renders darkmode', () => {
+  const wrapper = rtl.render(<useDarkMode/>);
   console.log(wrapper.debug());
 });
 
@@ -26,7 +39,7 @@ it('contains Kerr', () => {
 // it("renders without crashing", () => {
 //   const div = document.createElement("div");
 //   ReactDOM.render(<App/>, div);
-//   ReactDOM.unmountComanentAtNode(div);
+//   ReactDOM.un(div);
 // });
 // test('renders dashboard component', () => {
 //   const wrapper = rtl.render(<Display/>)
